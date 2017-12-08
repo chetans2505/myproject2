5e6614e3b72295ae5db5237d3f7dae639d6e23c7
chetans2505 <chetan.sg18@gmail.com>
2017-12-08 17:38:10 +0530

    commit

--- MyProject_Practice1/src/test/java/test/chet/TC01.java
+++ MyProject_Practice1/src/test/java/test/chet/TC01.java
@@ -3,20 +3,25 @@ package test.chet;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TC01 {
 	 WebDriver driver;
-
+	 @Parameters({"browser"})
 	@Test
-	public void test2(){
-
+	public void test2(String browser){
+		 if(browser.equalsIgnoreCase("firefox")){
+			System.out.println(browser+" browser version is not compatible "); 
+		 }
+		 else{
 		System.out.println("samp");
 		System.setProperty("webdriver.chrome.driver","D:\\chet1\\MyProject_Practice1\\drivers\\chromedriver.exe");
 		  driver = new ChromeDriver();
 		   driver.get("http://www.monster.com");
 		//driver=new FirefoxDriver();
 		   driver.close();
+		 }
 	}
 	
 }
